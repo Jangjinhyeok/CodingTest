b212c22b172e11a71d5881b96cebe6a92d2bf0a6
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-28 14:40:09 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    if(n < 7)
+        answer++;
+    else
+    {
+        answer =  n/7;   
+        if(n%7 != 0)
+            answer++;
+    }
+           
+    return answer;
+}
