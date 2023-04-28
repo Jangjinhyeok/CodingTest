8f8b10b3b352ee06dc53133d3508c9412d25af3f
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-28 12:00:23 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -1,11 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string my_string) {
     string answer = "";
-    for(int i = my_string.size()-1; i >= 0; i--)
-        answer += my_string[i];
-    return answer;
+    //algorithm 클래스 include 후에 answer = reverse(my_string.begin(),my_string.end())
+    /*for(int i = my_string.size()-1; i >= 0; i--)
+        answer += my_string[i];*/
+    reverse(my_string.begin(),my_string.end());
+    return my_string;
 }
