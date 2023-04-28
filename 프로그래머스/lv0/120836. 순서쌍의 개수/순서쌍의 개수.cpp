2dccf499f1a4a46a8085e837509e11ed8640a5af
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-28 15:28:55 +0900

    [level 0] Title: 순서쌍의 개수, Time: 3.49 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
+    /*
     vector<int> arr;
     for (int i = 1; i <= n; i++)
         if (n % i == 0) arr.push_back(i);
@@ -26,6 +27,10 @@ int solution(int n) {
         for (int j = i; j < arr.size()-i; j++)
             if (arr[i] * arr[j] == n) answer++;
     }
+    */
+    // 순서쌍의 개수는 n을 나눌 수 있는 수의 개수와 같음 그러니 다음과 같이 코드 수정
+    for (int i = 1; i <= n; i++)
+        if (n % i == 0) answer++;
 
     return answer;
 }
