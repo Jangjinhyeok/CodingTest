eb3e560011e170d59047075b8afede685fa06a52
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-30 00:09:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -9,6 +9,7 @@ vector<int> solution(string my_string) {
     for(auto i : my_string)
     {
         if(i > 47 && i < 58)
+            //i - '0' >> char to int
             answer.push_back(i - 48);
     }
     sort(answer.begin(),answer.end());
