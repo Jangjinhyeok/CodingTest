8e6b1e41a15948b5c92274e5ff7f139cb5f9de2e
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-30 00:32:33 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -5,15 +5,24 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
+    int answer = 0;
+    /*
     int answer = numbers[0] * numbers[1];
     
     for(int i = 0; i < numbers.size()-1; i++)
     {
         for(int j = i+1; j < numbers.size(); j++)
         {
+            if(i == 0 && j == 1)
+                continue;
             answer = max(answer,numbers[i] * numbers[j]);
         }
     }
+    */
+    //한 천재의 발상
+    sort(numbers.begin(),numbers.end());
+    int n = numbers.size();
+    answer = max(numbers[0] * numbers[1] , numbers[n -1] * numbers[n - 2]);
     
     return answer;
 }
