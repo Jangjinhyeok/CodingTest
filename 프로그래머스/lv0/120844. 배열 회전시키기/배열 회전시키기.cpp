cbdafc14e7505181364912a21de403a5042bc243
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-30 00:50:27 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 4.24 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, string direction) {
+    vector<int> answer;
+
+    if(direction[0] == 'r')
+    {
+        for(int i = numbers.size()-1; i > 0; i--)
+            swap(numbers[i],numbers[i-1]);
+
+    }
+    else
+    {
+        for(int i = 0; i < numbers.size()-1; i++)
+            swap(numbers[i],numbers[i+1]);
+    }
+    return numbers;
+}
