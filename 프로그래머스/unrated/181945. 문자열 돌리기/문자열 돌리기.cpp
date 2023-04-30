2e2d5fc83191015e5ea62e6ec6edcb4b113ee994
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-04-30 17:30:43 +0900

    [unrated] Title: 문자열 돌리기, Time: 2.84 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(auto i : str)
+        cout << i << endl;
+    return 0;
+}
