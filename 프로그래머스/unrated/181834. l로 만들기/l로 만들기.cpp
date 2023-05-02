d93857aa487c0a7dd0dd8c1757a17c8d991917fc
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-05-02 15:48:52 +0900

    [unrated] Title: l로 만들기, Time: 0.58 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
@@ -7,6 +7,7 @@ string solution(string myString) {
     string answer = "";
     for(auto i : myString)
     {
+        //아스키코드 숫자가 아니여도 i < 'l' 도 가능
         if(i < 108)
             answer.push_back('l');
         else
