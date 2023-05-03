c3e199e2748b5f220e8184a808514a58fdac92c2
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-05-03 10:08:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -8,18 +8,24 @@ int solution(string my_string) {
     string num = "";
     for(auto i : my_string)
     {
+        //숫자면 num에 넣기
         if(i > 47 && i < 58)
             num.push_back(i);
         else
         {
+            //num이 비었으면 그냥 진행
             if(num.empty())
                 continue;
+            //num을 int로 바꿔서 더해주기
             answer = answer + stoi(num);
+            //num 초기화
             num.clear();
         }
     }
-    
+    //반복문이 끝나고 num에 숫자가 남아있으면 마저 더해주기
     if(!num.empty())
         answer = answer + stoi(num);
+    
+    //stringstream 공부해서 다시 적용해볼것
     return answer;
 }
