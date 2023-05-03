8546b8f9f0dae18c9d78230004e4695f1e6a0dd0
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-05-03 11:12:39 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 1.33 ms, Memory: 4.75 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -18,5 +18,15 @@ vector<vector<int>> solution(int n) {
         answer.push_back(temp);
         temp.clear();
     }
+    
+    //아래 풀이로 이중백터 초기화 방법 참고
+    /*
+    vector<vector<int>> answer(n,vector<int>(n,0));
+
+    for(int i = 0; i < n; ++i)
+    {
+        answer[i][i] = 1;
+    }
+    */ 
     return answer;
 }
