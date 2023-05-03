3c95f2637a86f92f3200d52dd457567eaf3e2966
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-05-03 13:39:21 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.cpp
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.cpp
@@ -5,23 +5,8 @@ using namespace std;
 
 bool solution(bool x1, bool x2, bool x3, bool x4) {
     bool answer = true;
-    bool a1 = true;
-    bool a2 = true;
     
-    if(x1 || x2)
-        a1 = true;
-    else
-        a1 = false;
-    
-    if(x3||x4)
-        a2 = true;
-    else
-        a2 = false;
-    
-    if(a1&&a2)
-        answer = true;
-    else
-        answer = false;
+    answer = (x1|x2) & (x3|x4);
     
     return answer;
 }
