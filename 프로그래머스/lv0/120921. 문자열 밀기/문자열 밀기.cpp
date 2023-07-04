f0df24334b1a0024670b5b4dcbcdb116a9e9de2d
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-07-04 10:16:00 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string A, string B) {
+    int answer = -1;
+    if (A == B)
+        return answer = 0;
+
+    string temp = A;
+    string temp2 = temp;
+    for (int i = 0; i < A.size(); i++)
+    {
+        for (int j = 0; j < A.size(); j++)
+        {
+            if (j == 0)
+            {
+                temp[j] = temp2.back();
+                continue;
+            }
+
+            temp[j] = temp2[j-1];
+        }
+        
+        if (temp == B)
+        {
+            answer = i+1;
+            break;
+        }
+        temp2 = temp;
+    }
+    
+    
+    return answer;
+}
