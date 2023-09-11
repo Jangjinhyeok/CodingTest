453ba2c41cb82f1af79d50f890455fa4c7d4335d
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2023-09-11 10:29:05 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int sum = 0;
+	for (int i = 0; i < number.size()-2; i++)
+	{
+		sum = number[i];
+		for (int j = i + 1; j < number.size() - 1; j++)
+		{
+			sum += number[j];
+			for (int k = j + 1; k < number.size(); k++)
+			{
+				sum += number[k];
+				if (sum == 0)
+					answer++;
+				sum -= number[k];
+			}
+			sum -= number[j];
+		}
+	}
+    return answer;
+}
