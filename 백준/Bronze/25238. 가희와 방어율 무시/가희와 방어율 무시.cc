3501752e1dc3802144e67806b4fddedca45b9487
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-02-19 11:47:34 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.cc
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.cc
@@ -1,15 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	int a, b;
+	cin >> a >> b;
+	int temp = (a * (100 - b)) / 100;
 
-    int a, b;
-    cin >> a >> b;
 
-    int remaining = (a * (100 - b)) / 100; // 남은 체력 계산
-
-    cout << (remaining < 100 ? 1 : 0);
-    return 0;
+	if (temp < 100)
+		cout << 1;
+	else
+		cout << 0;
+	return 0;
 }
