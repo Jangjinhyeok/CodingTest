48488594738ba9fc22333d293ccd4efca0c9cb9b
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-03-23 21:52:38 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	int answer = 0;
+	answer = pow(2,n);
+    cout << answer;
+	
+	return 0;
+}
