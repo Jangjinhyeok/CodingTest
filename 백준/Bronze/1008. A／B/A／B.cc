5ca59ae1ab49c4b2946ce5d0d578cd8234650deb
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-03-29 22:42:08 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    double A, B;
+
+    cin >> A >> B;
+
+    cout.precision(10); 
+    cout << fixed;
+    cout << A / B << endl;
+
+	return 0;
+}
