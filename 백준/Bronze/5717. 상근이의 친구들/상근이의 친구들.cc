43f4f3eb5d6846d2ac138dc9db1e0760f2bbc6e7
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-04-14 18:40:13 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, f;
+    while(true)
+    {
+        cin >> m >> f;
+        if(m == 0 && f == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << m + f << "\n";
+        }
+        
+    }
+    return 0;
+}
