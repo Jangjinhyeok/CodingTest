e6ea4a2f2838f23bf8fb754b6133f264b0b68729
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-05-11 23:35:24 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    double answer = (a * 2) + (2 * 3.141592 * b);
+    cout << fixed << setprecision(6) << answer << endl;
+    return 0;
+}
