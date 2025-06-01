1560fd50e4172a06ae8119ee8de8cc97b4beeb26
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-06-01 23:21:45 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    double r;
+    cin >> r;
+    double eu = r * r * M_PI;
+    double ta = 2.0 * r * r;
+    cout << fixed << setprecision(6);
+    cout << eu << endl;
+    cout << ta << endl;
+    return 0;
+}
