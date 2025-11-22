7d3f9a1f047756f31685c3d8af4e1cf42e10e952
Jangjinhyeok <37360607+Jangjinhyeok@users.noreply.github.com>
2025-11-22 18:03:09 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -1,43 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-    int n;
-    cin >> n;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    vector<pair<int, int>> people(n);
+	int n;
+	cin >> n;
 
-    // 몸무게와 키 입력받기
-    for (int i = 0; i < n; i++)
-    {
-        cin >> people[i].first >> people[i].second;
-    }
+	int bigWeight = 0;
+	int bigTall = 0;
 
-    vector<int> ranking(n, 1);
+	vector<pair<int, int>> v;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j) continue;
+	for (int i = 1; i <= n; i++)
+	{
+		int weight;
+		int tall;
+		cin >> weight >> tall;
 
-            if (people[j].first > people[i].first && people[j].second > people[i].second)
-            {
-                ranking[i]++;
-            }
-        }
-    }
+		v.push_back({ weight, tall });
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << ranking[i];
-        if (i != n - 1)
-        {
-            cout << " ";
-        }
-    }
-    
-    return 0;
+	vector<int> ranking;
+	for (int i = 0; i < n; i++)
+	{
+		int rank = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (v[i].first < v[j].first && v[i].second < v[j].second)
+			{
+				rank++;
+			}
+		}
+		ranking.push_back(rank);
+	}
+
+	for (int i : ranking)
+	{
+		cout << i << " ";
+	}
+	return 0;
 }
